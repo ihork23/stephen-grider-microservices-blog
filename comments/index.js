23c0c4622c59fb9a5e76ce88419adfb4a8d7fd10
ihork <js.web23@gmail.com>
2022-12-28 14:35:55 +0100

    Create EventBus

--- comments/index.js
+++ comments/index.js
@@ -18,13 +18,25 @@ app.get('/posts/:id/comments', (req, res) => {
 app.post('/posts/:id/comments', (req, res) => {
   const commentId = randomBytes(4).toString('hex')
   const { content } = req.body
+  const postId = req.params.id
   
-  const comments = commentsByPostId[req.params.id] || []
+  const comments = commentsByPostId[postId] || []
 
   comments.push({ id: commentId, content })
   commentsByPostId[req.params.id] = comments
 
+  axios.post('http://localhost:4005/events', {
+    type: 'CommentCreated',
+    data: {id: commentId, content, postId}
+  })
+
   res.status(201).send(comments)
+})
+
+app.post('/events', (req, res) => {
+  console.log('Received', req.body.type)
+
+  res.send({})
 })
 
 app.listen(4001, () => {
---
+++ query/index.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+const app = express()
+app.use(bodyParser.json())
+app.use(cors())
+
+app.get('/posts', (req, res) => {
+  res.send(posts)
+})
+
+app.post('/posts', (req, res) => {
+  const { type, data } = req.body
+  
+  if (type === 'PostCreated') {
+    const { id, title } = data
+    
+    post[id] = {id, title, comments: []}
+  }
+
+  if (type === 'CommentCreated') {
+    const { id, content, postId } = data
+    
+    const post = posts[postId]
+    post.comments.push({id, content})
+  }
+
+  res.send({})
+})
+
+app.listen(4002, () => {})
